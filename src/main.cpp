833af0b0be15d6f67f4e75577039da8eed3a2102
Cristo <cgg.code@gmail.com>
2020-03-29 18:43:42 +0100

    Ahora es un rectangulo hurrayyyy

--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,25 @@
 
 int main()
 {
-  sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-  sf::CircleShape shape(100.f);
-  shape.setFillColor(sf::Color::Green);
+  unsigned int largo = 640;
+  unsigned int ancho = 480;
+  sf::RenderWindow window (sf::VideoMode(largo, ancho), "Hello World");
+  window.setFramerateLimit(60);
+  sf::RectangleShape shape(sf::Vector2f(100,50));
+  shape.setFillColor(sf::Color::Magenta);
+  shape.setPosition(largo/2.f, ancho/2.f);
 
+  window.clear();
+  window.draw(shape);
+  window.display();
   while (window.isOpen())
   {
     sf::Event event;
     while (window.pollEvent(event))
-    {
       if (event.type == sf::Event::Closed)
         window.close();
-    }
-
-    window.clear();
-    window.draw(shape);
-    window.display();
   }
+
+
   return 0;
 }
