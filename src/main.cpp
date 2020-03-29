d99d4785306dc3d48399037d92b6161fdc3672bd
Cristo <cgg.code@gmail.com>
2020-03-29 22:02:21 +0100

    Avances menores (sueño -> conseguir una hitbox con el suelo)

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,33 @@
 
 int main()
 {
-  unsigned int largo = 640;
-  unsigned int ancho = 480;
-  sf::RenderWindow window (sf::VideoMode(largo, ancho), "Hello World");
+  unsigned int anchura = 640;
+  unsigned int altura = 480;
+  sf::RenderWindow window (sf::VideoMode(anchura, altura), "A ver si me sale la hitbox");
   window.setFramerateLimit(60);
   sf::RectangleShape shape(sf::Vector2f(100,50));
   shape.setFillColor(sf::Color::Magenta);
-  shape.setPosition(largo/2.f, ancho/2.f);
+  unsigned int x = anchura/2, y = altura/2;
 
-  window.clear();
-  window.draw(shape);
-  window.display();
+  sf::RectangleShape suelo(sf::Vector2f(anchura/3,altura/3));
+  suelo.setPosition(anchura/2, altura/3);
+  suelo.setFillColor(sf::Color(165, 42, 42));
+
   while (window.isOpen())
   {
     sf::Event event;
     while (window.pollEvent(event))
       if (event.type == sf::Event::Closed)
         window.close();
+  if (sf::Keyboard::isKeyPressed (sf::Keyboard::Left))  x -= 10;
+  if (sf::Keyboard::isKeyPressed (sf::Keyboard::Right)) x += 10;
+  if (sf::Keyboard::isKeyPressed (sf::Keyboard::Up))    y -= 10;
+  if (sf::Keyboard::isKeyPressed (sf::Keyboard::Down))  y += 10;
+  shape.setPosition(x, y);
+  window.clear();
+  window.draw(shape);
+  window.draw(suelo);
+  window.display();
   }
 
 
