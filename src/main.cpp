d6c0753533e66ed6be926893f27de6b0f06fa5f2
Cristo <cgg.code@gmail.com>
2020-03-30 02:47:33 +0100

    Conseguido hitbox básica con suelo y paredes (Siguiente objetivo -> salto con física)

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,17 @@
 
 int main()
 {
-  unsigned int anchura = 640;
-  unsigned int altura = 480;
+  const unsigned int anchura = 500;
+  const unsigned int altura = 500;
   sf::RenderWindow window (sf::VideoMode(anchura, altura), "A ver si me sale la hitbox");
-  window.setFramerateLimit(60);
+  window.setFramerateLimit(30);
+
+  unsigned int x = 0, y = altura - altura/3 - 50;
   sf::RectangleShape shape(sf::Vector2f(100,50));
   shape.setFillColor(sf::Color::Magenta);
-  unsigned int x = anchura/2, y = altura/2;
 
-  sf::RectangleShape suelo(sf::Vector2f(anchura/3,altura/3));
-  suelo.setPosition(anchura/2, altura/3);
+  sf::RectangleShape suelo(sf::Vector2f(anchura, altura/3));
+  suelo.setPosition(0, altura - altura/3);
   suelo.setFillColor(sf::Color(165, 42, 42));
 
   while (window.isOpen())
@@ -20,10 +21,10 @@ int main()
     while (window.pollEvent(event))
       if (event.type == sf::Event::Closed)
         window.close();
-  if (sf::Keyboard::isKeyPressed (sf::Keyboard::Left))  x -= 10;
-  if (sf::Keyboard::isKeyPressed (sf::Keyboard::Right)) x += 10;
-  if (sf::Keyboard::isKeyPressed (sf::Keyboard::Up))    y -= 10;
-  if (sf::Keyboard::isKeyPressed (sf::Keyboard::Down))  y += 10;
+  if (sf::Keyboard::isKeyPressed (sf::Keyboard::Left) && x >= 10)  x -= 10;
+  if (sf::Keyboard::isKeyPressed (sf::Keyboard::Right) && x <= anchura - 100 - 10) x += 10;
+  // if (sf::Keyboard::isKeyPressed (sf::Keyboard::Up))    y -= 10; // why? Esta a la inversa
+  // if (sf::Keyboard::isKeyPressed (sf::Keyboard::Down))  y += 10;
   shape.setPosition(x, y);
   window.clear();
   window.draw(shape);
